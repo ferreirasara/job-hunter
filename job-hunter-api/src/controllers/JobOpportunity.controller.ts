cf319e793e9167464563e8dba3bd87c5fe343c6c
Sara Ferreira <ferreirasara1501@gmail.com>
2023-05-26 19:53:10 -0300

    feat(api): create update endpoint

--- job-hunter-api/src/controllers/JobOpportunity.controller.ts
+++ job-hunter-api/src/controllers/JobOpportunity.controller.ts
@@ -1,9 +1,20 @@
 import { AppDataSource } from "../data-source";
 import { JobOpportunity } from "../entity/JobOpportunity"
 
+type UpdateJobInput = {
+  uuid: string,
+  applied?: boolean,
+  discarded?: boolean,
+}
+
 export default class JobOpportunityController {
   public static async getAllJobs() {
     const jobs = await AppDataSource.manager.find(JobOpportunity);
     return jobs;
+  }
+
+  public static async updateJob(job: UpdateJobInput) {
+    const response = await AppDataSource.manager.update(JobOpportunity, job.uuid, { applied: job.applied, discarded: job.discarded });
+    return response?.affected > 0;
   }
 }
--- job-hunter-api/src/index.ts
+++ job-hunter-api/src/index.ts
@@ -4,6 +4,11 @@ import morgan = require("morgan")
 import { AppDataSource } from "./data-source"
 import JobOpportunityController from "./controllers/JobOpportunity.controller"
 
+type PostJobBody = {
+  applied?: boolean
+  discarded?: boolean
+}
+
 AppDataSource.initialize().then(async () => {
   const PORT = 8080
   const HOSTNAME = 'http://localhost'
@@ -11,10 +16,21 @@ AppDataSource.initialize().then(async () => {
 
   app.use(morgan('short'))
   app.use(cors())
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: false }));
 
   app.get('/jobs', async (req, res) => {
     const jobs = await JobOpportunityController.getAllJobs();
     res.send(jobs)
+  })
+
+  app.post('/job/:uuid', async (req, res) => {
+    const uuid = req.params.uuid;
+    const body: PostJobBody = req.body;
+
+    const updated = await JobOpportunityController.updateJob({ uuid, applied: body?.applied, discarded: body?.discarded })
+
+    res.send(updated);
   })
 
   app.listen(PORT, () => {
