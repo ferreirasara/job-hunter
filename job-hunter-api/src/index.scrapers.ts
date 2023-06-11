e0bca325eb810e6be84e6c79945b7f9c529fd28f
Sara Ferreira <ferreirasara1501@gmail.com>
2023-06-11 15:06:39 -0300

    feat(scrapers): minor improvements

--- job-hunter-api/src/index.scrapers.ts
+++ job-hunter-api/src/index.scrapers.ts
@@ -9,23 +9,23 @@ import VagasScraper from "./scrapers/vagas.scraper";
 AppDataSource.initialize().then(async () => {
   let numberOfNewJobs = 0;
 
-  const tramposScraper = new TramposScraper({ filterExistentsJobs: true })
-  numberOfNewJobs += await tramposScraper.saveJobs();
-
   const gupyScraper = new GupyScraper({ filterExistentsJobs: true })
   numberOfNewJobs += await gupyScraper.saveJobs();
+
+  const linkedinScraper = new LinkedinScraper({ filterExistentsJobs: true })
+  numberOfNewJobs += await linkedinScraper.saveJobs();
 
   const programathorScraper = new ProgramathorScraper({ filterExistentsJobs: true })
   numberOfNewJobs += await programathorScraper.saveJobs();
 
-  const vagasScraper = new VagasScraper({ filterExistentsJobs: true })
-  numberOfNewJobs += await vagasScraper.saveJobs();
-
   const remotarScraper = new RemotarScraper({ filterExistentsJobs: true })
   numberOfNewJobs += await remotarScraper.saveJobs();
 
-  const linkedinScraper = new LinkedinScraper({ filterExistentsJobs: true })
-  numberOfNewJobs += await linkedinScraper.saveJobs();
+  const tramposScraper = new TramposScraper({ filterExistentsJobs: true })
+  numberOfNewJobs += await tramposScraper.saveJobs();
+
+  const vagasScraper = new VagasScraper({ filterExistentsJobs: true })
+  numberOfNewJobs += await vagasScraper.saveJobs();
 
   console.log(`Number of new jobs: ${numberOfNewJobs}`);
 }).catch(error => console.log(error))
--- job-hunter-api/src/urls/linkedinUrls.ts
+++ job-hunter-api/src/urls/linkedinUrls.ts
@@ -1,6 +1,11 @@
 export const LINKEDIN_URLS = [
-  'https://www.linkedin.com/jobs/view/3602410518/?refId=bf48ab84-7a6b-49f8-9511-3839ea669daa&trackingId=DN27249LRMOtxkCJ8vMkcA%3D%3D&trk=flagship3_job_home_savedjobs&lipi=urn%3Ali%3Apage%3Ad_flagship3_myitems_savedjobs%3Bk5c7r8XHR26OFZryauV0ZA%3D%3D',
-  'https://www.linkedin.com/jobs/view/3598199568/?refId=bf48ab84-7a6b-49f8-9511-3839ea669daa&trackingId=aKJ1vW2FRlOdJc6Ms5Dwww%3D%3D&trk=flagship3_job_home_savedjobs&lipi=urn%3Ali%3Apage%3Ad_flagship3_myitems_savedjobs%3Bk5c7r8XHR26OFZryauV0ZA%3D%3D',
-  'https://www.linkedin.com/jobs/view/3598402119/?refId=eca0881f-46bd-47b3-814d-f168090a04b8&trackingId=kUzyDNoATgChiIiKI7nnZg%3D%3D',
-  'https://www.linkedin.com/jobs/view/3612645306/?refId=bf48ab84-7a6b-49f8-9511-3839ea669daa&trackingId=MJlRkkDCSRaNJVhp%2F4yhOQ%3D%3D&trk=flagship3_job_home_savedjobs&lipi=urn%3Ali%3Apage%3Ad_flagship3_myitems_savedjobs%3Bk5c7r8XHR26OFZryauV0ZA%3D%3D',
+  'https://www.linkedin.com/jobs/view/3602410518/',
+  'https://www.linkedin.com/jobs/view/3598199568/',
+  'https://www.linkedin.com/jobs/view/3598402119/',
+  'https://www.linkedin.com/jobs/view/3612645306/',
+  'https://www.linkedin.com/jobs/view/3580876857/',
+  'https://www.linkedin.com/jobs/view/3606648538/',
+  'https://www.linkedin.com/jobs/view/3574572360/',
+  'https://www.linkedin.com/jobs/view/3608998116/',
+  'https://www.linkedin.com/jobs/view/3612699087/',
 ]
