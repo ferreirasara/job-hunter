1a0b7e4c19485c30fa2832930a03d1d3fba9ae94
ferreirasara <ferreirasara1501@gmail.com>
2025-03-03 17:27:31 -0300

    feat(app): update react

--- job-hunter-app/src/index.tsx
+++ job-hunter-app/src/index.tsx
@@ -5,7 +5,6 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { FiltersContextProvider } from './context/FiltersContext';
 import { PaginationContextProvider } from './context/PaginationContext';
 import { ShowOnlyContextProvider } from './context/ShowOnlyContext';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -52,8 +51,3 @@ root.render(
     </FiltersContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
