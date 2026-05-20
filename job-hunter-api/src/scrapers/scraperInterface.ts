9a842debfab4eb5e78667a2d151532607ad63a87
Sara F <ferreirasara1501@gmail.com>
2026-05-20 20:49:02 -0300

    feat(scraper): make headless option configurable

--- job-hunter-api/src/scrapers/scraperInterface.ts
+++ job-hunter-api/src/scrapers/scraperInterface.ts
@@ -28,11 +28,13 @@ export default abstract class ScraperInterface {
   protected async getBrowser({
     abortScript,
     abortStyle,
+    headless = true,
   }: {
     abortScript?: boolean;
     abortStyle?: boolean;
+    headless?: boolean;
   }) {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless });
     const page = await browser.newPage();
     const ua =
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36';
