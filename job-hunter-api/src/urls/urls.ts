4f899cbc9ccacd24aa78586f2b7d9fd348713c61
ferreirasara <ferreirasara1501@gmail.com>
2026-08-04 18:27:19 +0100

    Remove duplicate LinkedIn job search URLs for Frontend, React, and Desenvolvedor positions

--- job-hunter-api/src/urls/urls.ts
+++ job-hunter-api/src/urls/urls.ts
@@ -1,10 +1,7 @@
 export const LINKEDIN_URLS = [
   'https://www.linkedin.com/jobs/search/?keywords=Frontend&f_WT=2&geoId=106057199',
-  'https://www.linkedin.com/jobs/search/?keywords=Frontend&f_WT=2&geoId=103644278',
   'https://www.linkedin.com/jobs/search/?keywords=React&f_WT=2&geoId=106057199',
-  'https://www.linkedin.com/jobs/search/?keywords=React&f_WT=2&geoId=103644278',
   'https://www.linkedin.com/jobs/search/?keywords=Desenvolvedor&f_WT=2&geoId=106057199',
-  'https://www.linkedin.com/jobs/search/?keywords=Desenvolvedor&f_WT=2&geoId=103644278',
 ];
 
 export const PROGRAMATHOR_URLS = [
